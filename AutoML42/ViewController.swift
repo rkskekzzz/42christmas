736deaa336174f7e46d47e0e146d3606a1c7dd3e
su <wkdlfflxh@naver.com>
2021-12-20 16:33:11 +0900

    Feat : test

---
+++ AutoML42/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  AutoML42
+//
+//  Created by su on 2021/12/18.
+//
+
+import UIKit
+import MLImage
+import MLKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    let picker = UIImagePickerController()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        picker.delegate = self
+        let image2 = UIImage(named: "test")
+        imageView.image = image2
+        titleLabel.text = "hello world"
+    }
+    
+    func openLibrary(){
+      picker.sourceType = .photoLibrary
+      present(picker, animated: false, completion: nil)
+    }
+    func openCamera(){
+        if(UIImagePickerController .isSourceTypeAvailable(.camera)) {
+        picker.sourceType = .camera
+        present(picker, animated: false, completion: nil)
+        }
+        else {
+            print("Camera not available")
+        }
+    }
+
+    // MARK: - IBActions
+
+    @IBAction func detectButtonTabbed(_ sender: Any) {
+        let image = VisionImage(image: imageView.image)
+        visionImage.orientation = image.imageOrientation
+    }
+    
+    @IBAction func addImage(_ sender: Any) {
+        let alert =  UIAlertController(title: "원하는 타이틀", message: "원하는 메세지", preferredStyle: .actionSheet)
+        let library =  UIAlertAction(title: "사진앨범", style: .default) { (action) in
+            print("in library")
+            self.openLibrary()
+        }
+
+        let camera =  UIAlertAction(title: "카메라", style: .default) { (action) in
+            print("in camera")
+            self.openCamera()
+        }
+
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+
+        alert.addAction(library)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension ViewController : UIImagePickerControllerDelegate,
+UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            print(image)
+            imageView.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
+
+
